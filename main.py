9bf81cb762dfaebfa1464346879b7dbc34b4b167
Hammad Hassan <hammadhassan319@gmail.com>
2025-08-11 00:33:29 +0800

    Students_classes_modified

--- main.py
+++ main.py
@@ -10,7 +10,23 @@ class Bus:
 
         self.number = number
         self.driver = driver
-
         self.students = []
+
+        # To Add Students to the bus
+
+        def add_student(self, student):
+
+            self.students.append(student)
+        
+        # To Show the current Students in the bus
+
+        def show_students(self):
+
+            if not self.students:
+                print("No students assigned yet.")
+            else:
+                for s in self.students:
+                    print(f"{s.name} (Grade {s.grade})")
+
         
         
