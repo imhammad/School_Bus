d62a93e4b38a4ecac2b95c094457ce3f7be02222
Hammad Hassan <hammadhassan319@gmail.com>
2025-08-11 00:28:33 +0800

    Classes_defined

---
+++ main.py
@@ -0,0 +1,16 @@
+
+class Student:
+    def __init__(self, name, grade):
+
+        self.name = name
+        self.grade = grade
+
+class Bus:
+    def __init__(self, number, driver):
+
+        self.number = number
+        self.driver = driver
+
+        self.students = []
+        
+        
